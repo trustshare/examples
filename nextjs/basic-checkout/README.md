0f9f38ba0e24a5891d0ba17ac41cf9a7347bc60a
Simon Holmes <srsholmes@gmail.com>
2022-04-28 12:47:42 +0100

    feat: updates

--- nextjs/basic-checkout/README.md
+++ nextjs/basic-checkout/README.md
@@ -1,2 +1,17 @@
-# trustshare api demos
-A collection of demos for the trustshare api using Next JS as a starter.
+# trustshare basic checkout
+
+
+## To install dependencies
+```bash
+npm i
+# or yarn
+yarn 
+```
+
+## To run the app in dev mode
+
+```bash
+npm run dev
+# or yarn
+yarn dev
+```
--- nextjs/basic-checkout/utils/constants.ts
+++ nextjs/basic-checkout/utils/constants.ts
@@ -1,4 +1,5 @@
 import { SettlementInput } from '@trustshare/api';
+import { SettlementType } from '@trustshare/api/dist/types/types';
 
 export const products = [
   {
@@ -8,6 +9,14 @@ export const products = [
     quantity: 1,
     imageSrc:
       'https://tailwindui.com/img/ecommerce-images/shopping-cart-page-04-product-01.jpg',
+    intent: {
+      type: 'escrow',
+      to: {
+        type: 'individual',
+        email: 'e2e+test+target+one@trustshare.co',
+        name: 'Joe Smith',
+      },
+    },
   },
   {
     name: 'Medium Stuff Satchel',
@@ -16,13 +25,23 @@ export const products = [
     quantity: 1,
     imageSrc:
       'https://tailwindui.com/img/ecommerce-images/shopping-cart-page-04-product-02.jpg',
+    intent: {
+      type: 'immediate',
+      to: {
+        type: 'individual',
+        email: 'e2e+test+target+two@trustshare.co',
+        name: 'Barry Bonds',
+      },
+    },
   },
 ];
 
 export const settlements: SettlementInput[] = products.map((product) => {
   return {
-    type: 'funding',
+    type: product.intent.type as SettlementType,
     amount: product.price,
     description: product.name,
+    to: product.intent.to,
+    summary: `${product.quantity} x ${product.color}`,
   };
 });
