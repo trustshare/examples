cbd003f68711326c3b74d0eff95a85f5615dec12
Simon Holmes <srsholmes@gmail.com>
2022-10-21 11:36:27 +0100

    chore: split intent and product data

--- examples/basic-checkout/pages/index.tsx
+++ examples/basic-checkout/pages/index.tsx
@@ -1,8 +1,12 @@
-import type { PaymentIntent } from '@trustshare/api';
+import { products } from '@package/components/src/constants';
+import type {
+  ParticipantType,
+  PaymentIntent,
+  SettlementType,
+} from '@trustshare/api';
 import ts from '@trustshare/api';
 import { Checkout } from 'components/Checkout';
 import type { GetServerSideProps, NextPage } from 'next';
-import { settlements } from 'utils/constants';
 
 const Home: NextPage<{ client_secret: string }> = ({ client_secret }) => {
   return <Checkout clientSecret={client_secret} />;
@@ -10,6 +14,26 @@ const Home: NextPage<{ client_secret: string }> = ({ client_secret }) => {
 
 export const getServerSideProps: GetServerSideProps = async ({ query }) => {
   const trustshare = ts(process.env.TRUSTSHARE_PRIVATE_API_KEY ?? '');
+
+  const intentData = [
+    {
+      type: 'immediate',
+      to: {
+        type: 'individual' as ParticipantType,
+        email: 'e2e+test+target+two@trustshare.co',
+        name: 'Barry Bonds',
+      },
+    },
+    {
+      type: 'escrow',
+      to: {
+        type: 'individual' as ParticipantType,
+        email: 'e2e+test+target+one@trustshare.co',
+        name: 'Joe Smith',
+      },
+    },
+  ];
+
   const {
     api: {
       v1: { createPaymentIntent },
@@ -24,7 +48,15 @@ export const getServerSideProps: GetServerSideProps = async ({ query }) => {
       type: 'individual',
       name: 'Simon Hopkins',
     },
-    settlements: settlements,
+    settlements: products.map((product, i) => {
+      return {
+        type: intentData[i].type as SettlementType,
+        amount: product.price,
+        description: product.name,
+        to: intentData[i].to,
+        summary: `${product.quantity} x ${product.color}`,
+      };
+    }),
   });
 
   return {
