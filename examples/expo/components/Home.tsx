a819c0c7c624a0184ab3775c29e5763f4377c136
Simon Holmes <srsholmes@gmail.com>
2023-05-11 09:32:46 +0100

    chore: remove dev stuff

--- examples/expo/components/Home.tsx
+++ examples/expo/components/Home.tsx
@@ -36,9 +36,6 @@ export const Home = ({ navigation }: Props) => {
       <View style={styles.container}>
         {paymentClientSecret && (
           <Checkout
-            options={{
-              __BASE_URL: ".nope.sh",
-            }}
             clientSecret={paymentClientSecret}
             onCancel={() => {
               console.log("Payment cancelled");
@@ -57,9 +54,6 @@ export const Home = ({ navigation }: Props) => {
         )}
         {verificationClientSecret && (
           <Verify
-            options={{
-              __BASE_URL: ".nope.sh",
-            }}
             clientSecret={verificationClientSecret}
             onComplete={(args) => {
               console.log("Verification complete!");
